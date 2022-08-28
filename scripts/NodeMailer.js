9c2e568ec14d91561ff69aedf0762ee9cfe6bae9
marcosdandrea <marcosdandrea@gmail.com>
2022-08-28 19:50:08 -0300

    first commit

---
+++ scripts/NodeMailer.js
@@ -0,0 +1,24 @@
+const { createTransport } = require("nodemailer")
+
+const transporter = createTransport({
+    host: 'smtp.ethereal.email',
+    port: 587,
+    auth: {
+        user: 'kian.wintheiser@ethereal.email',
+        pass: '6Dr14uYSnCZ7AM2Pup'
+    }
+});
+
+module.exports = class SendMail {
+    constructor(){
+
+    }
+
+    async sendMail(mailOptions){
+        try{
+            const info = await transporter.sendMail(mailOptions)
+        }catch(error){
+            console.log (error)
+        }
+    }
+}
